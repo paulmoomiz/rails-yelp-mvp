041c0ec5ad1e50c46d9c924ab6854d45805a93d2
paul sipasseuth <paulsipasseuth@gmail.com>
2020-08-13 08:51:16 +0100

    RAILS day 3 - challenge 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+
+Restaurant.create(name:"Viet Cafe", address:"34, Cogan Avenue ", phone_number: "02 35 67 38 90", category: "chinese", review: "2")
+Restaurant.create(name:"Moulin Rouge", address:"12 City Road ", phone_number: "02 35 67 38 90", category: "french", review: "5")
+Restaurant.create(name:"Belgo", address:"2, Roman Street", phone_number: "02 35 67 38 90", category: "belgian", review: "3")
+
+
+
+
+  # ["chinese", "italian", "japanese", "french", "belgian"]
+
